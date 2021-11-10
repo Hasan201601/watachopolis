700c7407abfba3a1269f48082e463c1c69bcee0b
Hasan201601 <hasanuzzaman201601@gmail.com>
2021-11-10 12:23:55 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_REACT_APP_API_KEY,
+    authDomain: process.env.FIREBASE_REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_REACT_APP_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_REACT_APP_APP_ID
+}
+export default firebaseConfig;
