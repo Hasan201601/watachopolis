c2ae29650e6a217461937efcb376799e528e8b11
Hasan201601 <hasanuzzaman201601@gmail.com>
2021-11-13 22:31:11 +0600

    updated

--- src/Pages/Catalog/CatalogBanner/CatalogBanner.js
+++ src/Pages/Catalog/CatalogBanner/CatalogBanner.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const CatalogBanner = () => {
     return (
-        <div style={{ backgroundImage: 'url(https://i.ibb.co/GWtszNt/background-footer.jpg)', height: '35vh', display: 'flex', alignItems: 'end', justifyContent: 'center' }}>
+        <div style={{ backgroundImage: 'url(https://i.ibb.co/GWtszNt/background-footer.jpg)', height: '35vh', display: 'flex', alignItems: 'end', justifyContent: 'center', backgroundAttachment: 'fixed' }}>
             <Typography variant="h4" className="Titillium" sx={{ color: 'white', width: { md: '40%', sm: '80%' }, pb: 4 }}>We Thrive to Build a Quality Collection House. See Our Latest Collections</Typography>
         </div >
     );
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Banner = () => {
     const imgUrl = "https://i.ibb.co/Kr2bG02/slider-1.jpg"
     return (
-        <div className="Titillium" style={{ backgroundImage: `url(${imgUrl})`, height: '90vh', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <div className="Titillium" style={{ backgroundImage: `url(${imgUrl})`, height: '90vh', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment: 'fixed' }}>
             <Container style={{ height: '100%' }}>
                 <div className="d-flex align-items-center justify-content-start" style={{ height: '100%' }}>
                     <div>
